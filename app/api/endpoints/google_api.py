f7a0cb57ebcbf47da162ab1f1911e54aa5b43a2d
Dmitriy Denisenko <denisenkodmi@mail.ru>
2024-09-29 20:22:39 +0300

    Flake8 issues have been fixed

--- app/api/endpoints/google_api.py
+++ app/api/endpoints/google_api.py
@@ -26,12 +26,11 @@ async def get_report(
     wrapper_services: Aiogoogle = Depends(get_service)
 ):
     """Только для суперюзеров!"""
-    # projects = await CRUDCharityProject.get_projects_by_completion_rate(
-    #     session
-    # )
-
     charity_project_crud = CRUDCharityProject(CharityProject)
-    projects = await charity_project_crud.get_projects_by_completion_rate(session)
+
+    projects = await charity_project_crud.get_projects_by_completion_rate(
+        session
+    )
 
     spreadsheet_id = await spreadsheets_create(wrapper_services)
 
